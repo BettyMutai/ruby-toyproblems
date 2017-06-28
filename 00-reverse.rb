3f8a6a0721832acca0ffd9f6dd841690a8cdb1b2
kitet <kitetnet@gmail.com>
2017-06-28 10:06:27 +0300

    worked on the first toy problem 00 reverse strings

--- 00-reverse.rb
+++ 00-reverse.rb
@@ -2,10 +2,17 @@
 # string with the same letters in reverse order.
 #
 # Difficulty: easy.
+#added the condition to check if value is a string or throw error 
 
-def reverse(string)
+def my_reverser(name)
+	if name.is_a? String 
+		name.reverse()
+	else
+		#throw and error
+		raise "Error occured"
+	end
 end
 
-puts("reverse(\"abc\") == \"cba\": #{reverse("abc") == "cba"}")
-puts("reverse(\"a\") == \"a\": #{reverse("a") == "a"}")
-puts("reverse(\"\") == \"\": #{reverse("") == ""}")
+my_reverser("Nicholas") #outputs "salohciN"
+my_reverser(75855) #throws an error
+
